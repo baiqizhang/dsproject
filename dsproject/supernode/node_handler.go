ac39b2b32e84187d3a60432f7527c6b15f595f75
surbhimotghare <surabhimotghare.com>
2016-04-10 22:23:11 -0400

    modifications

--- dsproject/supernode/node_handler.go
+++ dsproject/supernode/node_handler.go
@@ -6,11 +6,33 @@ import (
 	"fmt"
 	"io"
 	"strings"
+    "math"
 )
+
+//counter for carnodes which are ordinary nodes and counter for supernodes
+var countcar int = 0
+var countsuper int = 0
+var finalResult float = MaxFloat64
+var finalClient util.Client
 
 func handleNode(client util.Client) {
 	fmt.Println(client.Conn.RemoteAddr().String())
 	reader := bufio.NewReader(client.Conn)
+    
+    message, err := reader.ReadString('\n')
+		
+		// if connection comes from CarNode
+		if message == "CARNODE\n" {
+			fmt.Println("this is a " + message)
+			countcar++
+		}
+		
+        
+       // if connection comes from SuperNode
+		if message == "SUPERNODE\n" {
+			fmt.Println("this is a " + message)
+			countsuper++
+		} 
 
 	// Read handler
 	for {
@@ -25,10 +47,20 @@ func handleNode(client util.Client) {
 		words := strings.Split(message, " ")
 
 		// if connection comes from CarNode
-		if words[0] == "NAME" {
+		if words[0] == "COMPUTERESULT" {
 			fmt.Println("[Node] Register Name:" + words[1])
 			client.Name = words[1]
+            result, _ := strconv.ParseFloat(words[2], 64)
+            
+            if(result < finalResult){
+                finalResult = result
+                finalClient = client
+            }
+            
 		}
+       
+       
+        
 		fmt.Println("error: message not recognized")
 	}
 }
