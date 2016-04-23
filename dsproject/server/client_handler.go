3ed8ba4ae0962807674b9c8fa59d0b3f801149eb
baiqizhang <baiqiz@andrew.cmu.edu>
2016-04-23 12:00:35 -0700

    server check SN fail by heartbeat count

--- dsproject/server/client_handler.go
+++ dsproject/server/client_handler.go
@@ -7,13 +7,32 @@ import (
 	"io"
 	"net"
 	"strings"
+	"time"
 )
 
 var lastSNClient *util.Client // = nil
 var superNodeCount int
 
-// var superNodeAliveCounter = make(map[*util.Client]int)
+var superNodeAliveCounter = make(map[string]int)
 
+//check if connection from SN is broken
+func checkConnection() {
+	for {
+		for key, value := range superNodeAliveCounter {
+			superNodeAliveCounter[key] = value - 1
+			if util.Verbose == 1 {
+				fmt.Printf("Heartbeat count %s = %d\r\n", key, value)
+			}
+			if value == 0 {
+				delete(superNodeAliveCounter, key)
+				fmt.Println("[Supernode] Lost connection: " + key)
+			}
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+//handle message from a SN or CarNode
 func handleClient(client *util.Client) {
 	conn := client.Conn
 	fmt.Println("[New Client]:" + conn.RemoteAddr().String())
@@ -32,27 +51,29 @@ func handleClient(client *util.Client) {
 			fmt.Print("[Message Received]:" + message)
 		}
 
-		words := strings.Split(message, " ")
+		words := strings.Split(strings.Trim(message, "\r\n"), " ")
 
 		// if connection comes from CarNode
 		client.Type = words[0]
 		if words[0] == "SUPERNODE" {
+			newSNAddr := client.Conn.RemoteAddr()
+			newSNIPStr := newSNAddr.(*net.TCPAddr).IP.String()
+			newSNPortStr := words[2]
 			//mark the client as alive
-			// superNodeAliveCounter[&client] = 5
+			superNodeAliveCounter[newSNIPStr+":"+newSNPortStr] = 5
 
 			// check the 2nd arg
 			if words[1] == "REGISTER" {
-				client.Name = words[2]
+				client.Name = newSNPortStr
 				if lastSNClient != nil {
-					//tell the newcomer the last supernode's address
-					addr := lastSNClient.Conn.RemoteAddr()
-					writer.WriteString("PEERADDR " + addr.(*net.TCPAddr).IP.String() + ":" + lastSNClient.Name)
+					//tell the newcomer the last supernode's ip:port
+					lastSNAddr := lastSNClient.Conn.RemoteAddr()
+					writer.WriteString("PEERADDR " + lastSNAddr.(*net.TCPAddr).IP.String() + ":" + lastSNClient.Name)
 					writer.Flush()
 
 					if superNodeCount == 1 {
 						tempWriter := bufio.NewWriter(lastSNClient.Conn)
-						addr := client.Conn.RemoteAddr()
-						tempWriter.WriteString("PEERADDR " + addr.(*net.TCPAddr).IP.String() + ":" + words[2])
+						tempWriter.WriteString("PEERADDR " + newSNIPStr + ":" + newSNPortStr)
 						tempWriter.Flush()
 					}
 				}
--- dsproject/server/server.go
+++ dsproject/server/server.go
@@ -12,6 +12,93 @@ import (
 	"strings"
 )
 
+var clients []*util.Client
+var reqID int
+
+func main() {
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "-v" {
+			util.Verbose = 1
+		}
+	}
+
+	//start HTTP UI server at 8080
+	go listenHTTP()
+
+	// start TCP, listening at 7070
+	go listenTCP()
+
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter command: ")
+		cmd, _ := stdin.ReadString('\n')
+		processCommand(cmd)
+	}
+}
+
+// listen for SN/carN connection
+func listenTCP() {
+	listener, err := net.Listen("tcp", ":7070")
+	util.CheckError(err)
+
+	go checkConnection()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		newClient := util.Client{Conn: conn, Name: "none"}
+		clients = append(clients, &newClient)
+		go handleClient(&newClient)
+	}
+}
+
+// process command from Server Terminal
+func processCommand(cmd string) {
+	args := strings.Split(strings.Trim(cmd, "\r\n"), " ")
+
+	//Compute distance to the customer
+	if args[0] == "PICKUP" {
+		//Pickup the customer
+		source := util.ParseFloatCoordinates(args[1], args[2])
+		dest := util.ParseFloatCoordinates(args[3], args[4])
+		if source == nil || dest == nil {
+			fmt.Println("Error: incorrect PICKUP format:" + cmd)
+			return
+		}
+
+		var zero []byte
+		for _, client := range clients {
+			if client.Type == "NODE" {
+				continue
+			}
+			fmt.Println("client " + client.Type + " " + client.Name)
+			conn := client.Conn
+			fmt.Println(conn.RemoteAddr().String())
+			reader := bufio.NewReader(conn)
+			_, err := reader.Read(zero)
+			if err != nil {
+				continue
+			}
+
+			fmt.Println("[PICKUP] send to SN:" + client.Conn.RemoteAddr().String())
+			writer := bufio.NewWriter(conn)
+			writer.WriteString("PICKUP " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + strconv.Itoa(reqID) + "\n")
+			reqID++
+			writer.Flush()
+		}
+	}
+}
+
+func listenHTTP() {
+	http.Handle("/ride/", http.StripPrefix("/ride/", http.FileServer(http.Dir("./public"))))
+	http.HandleFunc("/api/data", dataHandler)
+	http.ListenAndServe(":8080", nil)
+	fmt.Print("web server running on 8080\n")
+}
+
 // Default HTTP Request Handler for UI
 func dataHandler(w http.ResponseWriter, r *http.Request) {
 	d := DataTable{
@@ -56,85 +143,4 @@ func dataHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Printf("%s\n", b)
 	fmt.Fprintf(w, "%s\n", b)
 	// fmt.Fprintf(w, "<h1>Hello from Team 9 %s!</h1>", r.URL.Path[1:])
-}
-
-var clients []*util.Client
-var reqID int
-
-func main() {
-	args := os.Args[1:]
-	for _, arg := range args {
-		if arg == "-v" {
-			util.Verbose = 1
-		}
-	}
-
-	//start HTTP UI server at 8080
-	go func() {
-		http.Handle("/ride/", http.StripPrefix("/ride/", http.FileServer(http.Dir("./public"))))
-		http.HandleFunc("/api/data", dataHandler)
-		http.ListenAndServe(":8080", nil)
-	}()
-
-	fmt.Print("web server running on 8080\n")
-
-	// start TCP, listening at 7070
-	go func() {
-		listener, err := net.Listen("tcp", ":7070")
-		util.CheckError(err)
-
-		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				continue
-			}
-			newClient := util.Client{Conn: conn, Name: "none"}
-			clients = append(clients, &newClient)
-			go handleClient(&newClient)
-		}
-	}()
-
-	stdin := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Enter command: ")
-		cmd, _ := stdin.ReadString('\n')
-		processCommand(cmd)
-	}
-}
-
-// process command from Server Terminal
-func processCommand(cmd string) {
-	args := strings.Split(strings.Trim(cmd, "\r\n"), " ")
-
-	//Compute distance to the customer
-	if args[0] == "PICKUP" {
-		//Pickup the customer
-		source := util.ParseFloatCoordinates(args[1], args[2])
-		dest := util.ParseFloatCoordinates(args[3], args[4])
-		if source == nil || dest == nil {
-			fmt.Println("Error: incorrect PICKUP format:" + cmd)
-			return
-		}
-
-		var zero []byte
-		for _, client := range clients {
-			if client.Type == "NODE" {
-				continue
-			}
-			fmt.Println("client " + client.Type + " " + client.Name)
-			conn := client.Conn
-			fmt.Println(conn.RemoteAddr().String())
-			reader := bufio.NewReader(conn)
-			_, err := reader.Read(zero)
-			if err != nil {
-				continue
-			}
-
-			fmt.Println("[PICKUP] send to SN:" + client.Conn.RemoteAddr().String())
-			writer := bufio.NewWriter(conn)
-			writer.WriteString("PICKUP " + args[1] + " " + args[2] + " " + args[3] + " " + args[4] + " " + strconv.Itoa(reqID) + "\n")
-			reqID++
-			writer.Flush()
-		}
-	}
 }
